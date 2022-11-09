f5437321b232b79ce731e7b3dbff66cad9823d61
Mikal <Johnsonmikal24@gmail.com>
2022-11-09 00:18:05 -0800

    Created Typescript file, started form validation

--- ts/main.ts
+++ ts/main.ts
@@ -1,3 +1,9 @@
 window.onload = function() {
-    let formBtn = document.querySelector("form > button");
+    let formBtn = 
+        <HTMLElement>document.querySelector("form > button");
+    formBtn.onclick = main;
+}
+
+function main():void{
+    alert("Reg button was clicked!");
 }
